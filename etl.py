d905426249edc6d452a35fefa1c65238119476d7
Ernâni de Britto Murtinho <ernanibmurtinho@gmail.com>
2020-10-22 01:40:28 -0300

    Update etl.py

--- etl.py
+++ etl.py
@@ -1,22 +1,57 @@
+from time import time
 import configparser
 import psycopg2
-from sql_queries import copy_table_queries, insert_table_queries
+import matplotlib.pyplot as plt
+import pandas as pd
+from sql_queries import copy_table_queries, insert_table_queries, insert_table
 
+config = configparser.ConfigParser()
+config.read('dwh.cfg')
 
 def load_staging_tables(cur, conn):
-    for query in copy_table_queries:
-        cur.execute(query)
-        print("commit!")
+    loadTimes = []
+    
+    for schema, table, s3path in copy_table_queries:
+        SQL_SET_SCHEMA = "SET search_path TO {};".format(schema)
+        cur.execute(SQL_SET_SCHEMA)
+        SQL_COPY = ("""                    
+                    copy {}.{} from {} 
+                    credentials 'aws_iam_role={}'
+                    region 'us-west-2' format as json 'auto'
+                            """).format(schema, table, config.get("S3", s3path), config.get("DWH", "DWH_ROLE_ARN"))
+
+        print("======= LOADING STAGING TABLE: ** {} ** IN SCHEMA ==> {} =======".format(table, schema))
+        print(SQL_COPY)
+
+        t0 = time()
+        cur.execute(SQL_COPY)
         conn.commit()
-    print("Tables loaded!")
+        tables = []
+        tables.append(table)
+        loadTime = time()-t0
+        loadTimes.append(loadTime)
+
+        print("=== DONE IN: {0:.2f} sec\n".format(loadTime))
+    return pd.DataFrame({"table":str(tables), "loadtime_":loadTimes}).set_index('table')
+
 
 def insert_tables(cur, conn):
-    for query in insert_table_queries:
+    
+    insertTimes = []
+    
+    for (query, table) in zip( insert_table_queries, insert_table ) :
+        
+        print("======= INSERTING DATA INTO TABLE: ** {} ** =======".format(table))
+        t0 = time()
         cur.execute(query)
-        print("commit!")
         conn.commit()
-    print("Data Inserted!")
+        insertTime = time()-t0
+        insertTimes.append(insertTime)
+        
+        print("=== DONE IN: {0:.2f} sec\n".format(insertTime))
+    return pd.DataFrame({"table":insert_tables, "inserttime_":insertTimes}).set_index('table')
 
+    
 def main():
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
@@ -24,7 +59,9 @@ def main():
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
     
-    load_staging_tables(cur, conn)
+    tbStats = load_staging_tables(cur, conn)
+    tbStats.plot.bar()
+    plt.show()
     insert_tables(cur, conn)
 
     conn.close()
